4f2e9a6ddd802d324832bcb093803ff87ff58263
Brian Talley <btalley@photobucket.com>
2014-02-19 09:29:53 -0700

    Adding files missed in last commit

--- functions.php
+++ functions.php
@@ -1,12 +1,19 @@
 <?php
 
-function getTraps($where) {
+function highlightWords($string,$words) {
+    foreach ( $words as $word ) {
+        $string = str_ireplace($word, '<span class="help-inline">'.$word.'</span>', $string);
+    }
+    return $string;
+}
 
-	$where_string = implode(" AND ", $where);
+function getTraps($where,$bool="AND",$low_limit,$high_limit) {
+
+	$where_string = implode(" $bool ", $where);
         if ($where_string) { $where_string = "WHERE " . $where_string; }
 
-    $query = "SELECT * FROM snmptt $where_string ORDER BY traptime DESC LIMIT 1000";
-    //print $query;
+    $query = "SELECT * FROM snmptt $where_string ORDER BY traptime DESC LIMIT $low_limit, $high_limit";
+    //print $query . "<br />";
     $result = mysql_query($query);
     $num_results = mysql_numrows($result);
     // fetch all rows and store in $r array
@@ -15,7 +22,45 @@ function getTraps($where) {
     // for some reason this returns an extra, empty array element... pop it to delete
     array_pop($r);
     return $r;
-
 }
 
+function getUnknownTraps($where,$bool="AND",$low_limit,$high_limit) {
+
+	$where_string = implode(" $bool ", $where);
+        if ($where_string) { $where_string = "WHERE " . $where_string; }
+
+    $query = "SELECT * FROM snmptt_unknown $where_string ORDER BY traptime DESC LIMIT $low_limit, $high_limit";
+    //print $query . "<br />";
+    $result = mysql_query($query);
+    $num_results = mysql_numrows($result);
+    // fetch all rows and store in $r array
+    $r = array();
+    while ($r[] = mysql_fetch_assoc($result));
+    // for some reason this returns an extra, empty array element... pop it to delete
+    array_pop($r);
+    return $r;
+}
+
+function getTrapCount($where,$bool="AND") {
+
+	$where_string = implode(" $bool ", $where);
+        if ($where_string) { $where_string = "WHERE " . $where_string; }
+
+    $query = "SELECT count(id) as count FROM snmptt $where_string";
+    //print $query;
+    $result = mysql_query($query);
+    $count = mysql_fetch_assoc($result);
+    return $count['count'];
+}
+function getUnknownTrapCount($where,$bool="AND") {
+
+	$where_string = implode(" $bool ", $where);
+        if ($where_string) { $where_string = "WHERE " . $where_string; }
+
+    $query = "SELECT count(trapoid) as count FROM snmptt_unknown $where_string";
+    //print $query;
+    $result = mysql_query($query);
+    $count = mysql_fetch_assoc($result);
+    return $count['count'];
+}
 ?>
--- index.php
+++ index.php
@@ -1,93 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+
+    <title>muckrake | traps</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/navbar-fixed-top.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <b><a class="navbar-brand" href="./">muckrake</a></b>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Traps</a></li>
+            <li><a href="unknown.php">Unknown Traps</a></li>
+          </ul>
+            <form class="navbar-form navbar-right" role="search" method="get" action="">
+        <div class="form-group">
+        <input type="text" class="form-control" placeholder="Search" name="f" value="<?php echo ($_GET['f'] ?: '') ?>"></th>
+      </div>
+      <button type="submit" class="btn btn-default" name="submit" value="Go">Go</button>&nbsp;&nbsp;&nbsp;
+      </form>
+    </div><!--/.nav-collapse -->
+  </div>
+
+    <div class="container-fluid">
+
 <?php
-# vim: sw=4 sts=4:
-#
-#
-# muckrake: snmptt web gui
-#
-# Copyright (C) 2014 Photobucket <btalley@photobucket.com>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
-
-
-echo "<html><head>";
-echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">";
-echo "<script src=\"sorttable.js\"></script>";
-echo "</head>";
-echo "<body>";
-echo "<h1>muckrake</h1>";
 
 include 'db.php';
 include 'functions.php';
 
-$where = array();
-if ($_GET['submit'] == "Submit") {
-	if ($_GET['hostname_filter'] != '') {
-		array_push($where, "hostname REGEXP '" . $_GET['hostname_filter'] . "'");
-	}
-	if ($_GET['trapoid_filter'] != '') {
-		array_push($where, "trapoid REGEXP '" . $_GET['trapoid_filter'] . "'");
-	}
-	if ($_GET['desc_filter'] != '') {
-		array_push($where, "formatline REGEXP '" . $_GET['desc_filter'] . "'");
-	}
-	if ($_GET['sev_filter'] != '') {
-		array_push($where, "severity REGEXP '" . $_GET['sev_filter'] . "'");
-	}
+$pag = 25;
+
+// get pagination limits
+if ($_GET['p']) {
+  $current_page = $_GET['p'];
+  $upper_limit = $pag * $_GET['p'];
+  $lower_limit = $upper_limit - $pag;
+} else {
+  $current_page = 1;
+  $lower_limit = 0;
+  $upper_limit = $pag;
 }
 
-$traps = getTraps($where);
+// rebuild current uri string
+$uri = '?';
+if (!empty($_GET['f'])) { $uri .= "f=" . $_GET['f']; }
 
-echo "<form name=\"input\" action=\"\" method=\"get\">";
-echo "<table class=\"sortable\">
-<tr>
-<th></th>
+$where = array();
+if (!empty($_GET['f'])) {
+  array_push($where, "hostname REGEXP '" . $_GET['f'] . "'");
+  array_push($where, "trapoid REGEXP '" . $_GET['f'] . "'");
+  array_push($where, "formatline REGEXP '" . $_GET['f'] . "'");
+  array_push($where, "severity REGEXP '" . $_GET['f'] . "'");
+  $traps = getTraps($where,"OR",$lower_limit,$pag);
+  $total_count = getTrapCount($where,"OR",0,100000000);
+} else {
+  $traps = getTraps($where,"AND",$lower_limit,$pag);
+  $total_count = getTrapCount($where,"AND",0,100000000);
+}
+
+print "<div class=\"well well-sm\">Total number of traps in db matching filter: <span class=\"label label-success\">$total_count</span>";
+print "<span class=\"pull-right\">Showing <span class=\"badge\">" . ($lower_limit+1) . " - " .($lower_limit+1+$pag). "</span></span></div>";
+
+?>
+
+<table class="table table-striped table-condensed table-hover">
+<thead><tr>
+<th>#</th>
 <th>hostname</th>
 <th>oid</th>
 <th>desc</th>
 <th>severity</th>
 <th>traptime</th>
-</tr>";
-echo "
-<tr>
 <th></th>
-<th><input type=\"text\" name=\"hostname_filter\" value=\"" . ($_GET['hostname_filter'] ?: '') .  "\"></th>
-<th><input type=\"text\" name=\"trapoid_filter\" value=\"" . ($_GET['trapoid_filter'] ?: '') .  "\"></th>
-<th><input type=\"text\" name=\"desc_filter\" value=\"" . ($_GET['desc_filter'] ?: '') .  "\"></th>
-<th><input type=\"text\" name=\"sev_filter\" value=\"" . ($_GET['sev_filter'] ?: '') .  "\"></th>
-<th></th>
-</tr>";
+</tr>
+              </thead>
 
-print count($traps) . " traps";
+              <tbody>
+
+<?php
+
+
 $i = 1;
 foreach ($traps as $item) {
-	if ($i % 2 == 0) { $even = 'even'; } else { $even = ''; }
-	print "<tr class=\"$even\"><td align=right>$i</td>";
-	print "<td nowrap>" . $item['hostname'] . "</td>";
-	print "<td nowrap>" . $item['trapoid'] . "</td>";
-	print "<td>" . nl2br($item['formatline']) . "</td>";
-	$severity = strtolower($item['severity']);
-	print "<td nowrap class=\"$severity\">" . $item['severity'] . "</td>";
-	print "<td align=right nowrap>" . $item['traptime'] . "</td>";
+        print "<tr><td align=right>$i</td>";
+        print "<td nowrap>" . $item['hostname'] . "</td>";
+        print "<td nowrap>" . $item['trapoid'] . "</td>";
+        print "<td>" . nl2br($item['formatline']) . "</td>";
+        $severity = strtolower($item['severity']);
 
-	print "</tr>";
-	$i++;
+	//if ($severity == "normal") { $severity = 'danger'; }
+
+        print "<td nowrap class=\"$severity\">";
+	//print $item['severity'];
+
+	if ($severity == "normal") { print "<span class=\"label label-info\">Normal</span>"; }
+	elseif ($severity == "informational") { print "<span class=\"label label-info\">Info</span>"; }
+
+	print "</td>";
+        print "<td align=right nowrap>" . $item['traptime'] . "</td>";
+        print "<td><a href=\"detail.php?id=" . $item['id'] . "\"><span class=\"glyphicon glyphicon-search\"></span></a>";
+        print "</tr>";
+        $i++;
 
 }
-echo "</table>";
 
-echo "<input type=\"submit\" name=\"submit\" value=\"Submit\">";
-
-echo "</html>";
 ?>
+	</tbody>
+	</table>
+
+<!--
+<span class="label label-default">Normal</span>
+<span class="label label-primary">Primary</span>
+<span class="label label-success">Success</span>
+<span class="label label-info">Info</span>
+<span class="label label-warning">Warning</span>
+<span class="label label-danger">Danger</span>
+<br />
+-->
+<div class="pull-right">
+<ul class="pagination pagination-lg">
+<?php
+//build pagination block
+
+// find first numbered page link
+$first = $current_page - 2;
+while ($first <= 0) {
+	$first++;
+}
+
+// handle "previous" button
+if ($current_page > 1) {
+    $prev_uri = $uri . "&p=" . ($current_page-1);
+    echo "<li><a href=\"$prev_uri\">&laquo;</a></li>";
+} else {
+    echo "<li class=\"disabled\"><a href=\"\">&laquo;</a></li>";
+}
+
+$num = $first;
+for ($i=0;$i<5;$i++) {
+	$new_uri = $uri . "&p=$num";
+	if ($num == $current_page) {
+		echo "<li class=\"active\"><a href=\"$new_uri\">$num</a></li>";
+	} else {
+		echo "<li><a href=\"$new_uri\">$num</a></li>";
+	}
+	$num++;
+}
+	$new_uri = $uri . "&p=$num";
+
+
+$next_uri = $uri . "&p=" . ($current_page+1);
+?>
+
+  <li><a href="<?php echo $next_uri; ?>">&raquo;</a></li>
+</ul>
+</div>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
